6a368c8a7dc2e9df1b79914c171ae7d1ade8933c
Marcin Powroźnik <biuro@mpowroznik.com>
2018-09-01 09:38:44 +0200

    add: getting data from object

--- src/sl-shoplist.js
+++ src/sl-shoplist.js
@@ -1,6 +1,7 @@
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import '@polymer/paper-checkbox/paper-checkbox.js';
 import './shared-styles.js';
+import '@polymer/polymer/lib/elements/dom-repeat.js';
 
 class SLView extends PolymerElement {
     static get template() {
@@ -8,10 +9,24 @@ class SLView extends PolymerElement {
         <div class="SLView">
             <h1>Lista zakupów</h1>
             <ul>
-                <paper-checkbox> pomidory</paper-checkbox>
-                <paper-checkbox> bułki</paper-checkbox>
+                <template is="dom-repeat" items="{{shoppinglist}}">
+                    <li>[[item.sl]]</li>
+                </template>
             </ul>
         </div>`;
+    }
+    static get properties() {
+        return {
+            shoppinglist:{
+                type: Array,
+                value() {
+                    return [
+                        {sl: 'bułki'},
+                        {sl: 'mleko'}
+                    ]
+                }
+            }
+        }
     }
 }
 
