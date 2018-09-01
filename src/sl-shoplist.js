1807a03b52dbbc725e0a3779abe1f5f527bbd7f5
Marcin Powroźnik <biuro@mpowroznik.com>
2018-09-01 02:01:24 +0200

    fix: paper-checkbox issue

--- src/sl-shoplist.js
+++ src/sl-shoplist.js
@@ -1,4 +1,5 @@
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+import '@polymer/paper-checkbox/paper-checkbox.js';
 import './shared-styles.js';
 
 class SLView extends PolymerElement {
@@ -7,8 +8,8 @@ class SLView extends PolymerElement {
         <div class="SLView">
             <h1>Lista zakupów</h1>
             <ul>
-                <li>pomidory</li>
-                <li>bułki</li>
+                <paper-checkbox> pomidory</paper-checkbox>
+                <paper-checkbox> bułki</paper-checkbox>
             </ul>
         </div>`;
     }
